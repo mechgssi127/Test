8b4eeb9f5ae9641594244b3182d1a6ff5ea13fb3
mechgssi127 <mechgssi127@gmail.com>
2026-03-01 07:55:26 -0600

    fix:fix the product item animation from delay to duration

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,9 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 export default [
   { ignores: ['dist'] },
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -6,8 +6,8 @@ const Item = ({product}) => {
     <div className='group relative'>
       <Link to={`/products/${product?.id}`}>
       <div className='aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-800/50 lg:aspect-none grup-hover:opacity-75 lg:h-80 h-96 border border-gray-700/50 relative'>
-        <img src={product?.image} alt={product?.name} className='h-full w-full object-cover object-center lg:h-full lg:w-full transition-transform group-hover:scale-110 delay-500 ease-out' />
-        <div className='absolute inset-0 bg-gradient-to-b from-gray-600/50 to-transparent opacity-0 group-hover:opacity-100 transition-opacity delay-500 ease-out pointer-events-none'></div>
+        <img src={product?.image} alt={product?.name} className='h-full w-full object-cover object-center lg:h-full lg:w-full transition-transform group-hover:scale-110 duration-500 ease-out' />
+        <div className='absolute inset-0 bg-gradient-to-b from-gray-600/50 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-500 ease-out pointer-events-none'></div>
       </div>
       </Link>
       <div className='mt-4 flex justify-between'>
