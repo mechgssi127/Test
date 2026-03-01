3703069393fcbc4f063783f0219bedcaf4df5e30
mechgssi127 <mechgssi127@gmail.com>
2026-03-01 07:40:13 -0600

    style:update stars to yellow

--- src/components/ProductDisplay.jsx
+++ src/components/ProductDisplay.jsx
@@ -23,10 +23,10 @@ const ProductDisplay = (props) => {
             <div className='flex md:1/2 flex-col mt-8 md:mt-0'>
                 <h1 className='text-foreground text-4xl font-bold'>{product.name}</h1>
                 <div className='flex items-center gap-1 text-gray-300 text-lg mt-4'>
-                    <Star fill='#138695' />
-                    <Star fill='#138695' />
-                    <Star fill='#138695' />
-                    <Star fill='#138695' />
+                    <Star fill='yellow' />
+                    <Star fill='yellow' />
+                    <Star fill='yellow' />
+                    <Star fill='yellow' />
                     <Star fill='gray' />
                     <p>(122)</p>
                 </div>
